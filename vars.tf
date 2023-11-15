50627ed21a39acff290e2fdc33393f556ad29fc2
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-15 09:53:00 +0530

    Terraform: Roboshop-App Terraform

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "tags" {}
+variable "internal" {}
+variable "lb_type" {}
+variable "vpc_id" {}
+variable "sg_port" {}
+variable "sg_ingress_cidr" {}
+variable "subnets" {}
