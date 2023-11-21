9b6a74784d5a032ccdeb804fe8af6cd16341874c
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-21 11:08:53 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-resource "aws_lb" "main" {
-  name               = local.lb_name
-  internal           = var.internal
-  load_balancer_type = var.lb_type
-  security_groups    = [aws_security_group.main.id]
-  subnets            = var.subnets
-  tags               = merge(local.tags, { Name = "${var.env}-alb" })
-}
-
 # Security Group creation
 resource "aws_security_group" "main" {
   name        = local.sg_name
@@ -28,5 +19,30 @@ resource "aws_security_group" "main" {
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = local.lb_name
+  internal           = var.internal
+  load_balancer_type = var.lb_type
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+  tags               = merge(local.tags, { Name = "${var.env}-alb" })
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "ERROR"
+      status_code  = "404"
+    }
   }
 }
